b70c1dac44bd4fa1ebc87cc1a5b327a2f479b8f0
Vladyslav Ruhalov <rugalyov211@gmail.com>
2023-09-13 18:06:40 +0300

    Add method turnToTree() for Grass

--- src/main/java/com/ruhalov/entity/objects/Grass.java
+++ src/main/java/com/ruhalov/entity/objects/Grass.java
@@ -1,10 +1,24 @@
 package com.ruhalov.entity.objects;
 
 import com.ruhalov.Coordinates;
+import com.ruhalov.World;
 import com.ruhalov.entity.Entity;
+import com.ruhalov.entity.objects.stationary.Tree;
 
 public class Grass extends Entity {
+    private int age = 0;
     public Grass(Coordinates coordinates) {
         this.coordinates = coordinates;
+    }
+    public void turnToTree(World world) {
+        int ageForTransformToTree = 9;
+        if (age == ageForTransformToTree) {
+            world.clearCell(coordinates);
+            Tree tree = new Tree(coordinates);
+            world.setEntity(tree);
+            age = 0;
+        } else {
+            age++;
+        }
     }
 }
