f7f9f57a81c23311324762216c1cc5951086e0b4
Vladyslav Ruhalov <rugalyov211@gmail.com>
2023-09-18 12:14:05 +0300

    Replace hardcode symbols in ConsoleRenderer to Constants values

--- src/main/java/com/ruhalov/ConsoleRenderer.java
+++ src/main/java/com/ruhalov/ConsoleRenderer.java
@@ -10,37 +10,34 @@ import com.ruhalov.entity.objects.stationary.Tree;
 public class ConsoleRenderer {
     public void render(World world) {
         for (int i = 0; i < world.getHeight(); i++) {
+            System.out.print(Constants.ANSI_WHITE_BACKGROUND);
             for (int j = 0; j < world.getWidth(); j++) {
                 Coordinates coordinates = new Coordinates(j, i);
                 int numOfEntity = world.getAllEntities().size();
                 for (Entity ent : world.getAllEntities()) {
                     if (ent.getCoordinates().equals(coordinates)) {
                         if (ent instanceof Predator) {
-                            System.out.print("\033[0;100m\uD83D\uDFE5");
-                            System.out.print("\033[0m");
+                            System.out.print(Constants.PREDATOR_SYMBOL);
                         } else if (ent instanceof Herbivore) {
-                            System.out.print("\033[0;100m\uD83D\uDFE8");
-                            System.out.print("\033[0m");
+                            System.out.print(Constants.HERBIVORE_SYMBOL);
                         } else if (ent instanceof Grass) {
-                            System.out.print("\033[0;100m\uD83D\uDFE2");
-                            System.out.print("\033[0m");
+                            System.out.print(Constants.GRASS_SYMBOL);
                         } else if (ent instanceof Rock) {
-                            System.out.print("\033[0;100m⚫");
-                            System.out.print("\033[0m");
+                            System.out.print(Constants.ROCK_SYMBOL);
                         } else if (ent instanceof Tree) {
-                            System.out.print("\033[0;100m\uD83D\uDFE4");
-                            System.out.print("\033[0m");
+                            System.out.print(Constants.TREE_SYMBOL);
                         }
                     } else {
                         numOfEntity--;
                         if (numOfEntity == 0) {
-                            System.out.print("\033[0;100m⬜");
-                            System.out.print("\033[0m");
+                            System.out.print(Constants.EMPTY_CELL_SYMBOL);
                         }
                     }
                 }
             }
-            System.out.println();
+
+            System.out.println(Constants.ANSI_RESET);
         }
+        System.out.println();
     }
 }
